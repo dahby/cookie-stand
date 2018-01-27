d770b6aab3a3f915922a39df0b98236b5b7e1ddb
dahby <dstoll243@gmail.com>
2018-01-27 14:38:57 -0800

    I have a table header

--- js/app.js
+++ js/app.js
@@ -5,12 +5,7 @@ var allLocations = [];
 var totalCookiesHr = 0;
 var netTotal = 0;
 
-function MakeLocation(
-  name,
-  minCustHr,
-  maxCustHr,
-  avgSalePerCust
-) {
+function MakeLocation(name, minCustHr, maxCustHr, avgSalePerCust) {
   this.name = name;
   this.minCustHr = minCustHr;
   this.maxCustHr = maxCustHr;
@@ -18,218 +13,45 @@ function MakeLocation(
   this.randCustHr = [];
   this.cookiesHr = [];
   this.totalCookies = 0;
-  allLocations.push(this);
+  allLocations.push(this);  
+
+  this.calcRandCustHr = function() {
+    for(var i = 0; i < hours.length; i++) {
+      this.randCustHr.push(Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr);
+    console.log(this.randCustHr[i]);
+     }
+    }
+     
+  this.calcCookiesHr = function() {
+    for (var j=0; j < hours.length; j++) {
+      this.cookiesHr.push(Math.round(this.avgSalePerCust * this.randCustHr[j]));
+    console.log(this.cookiesHr[j]);
+     }
+    }
+  
+  
+  };
+
+function makeStores() {
+  new MakeLocation("First and Pike", 23, 65, 6.3);
+  new MakeLocation("SeaTac Airport", 3, 24, 1.2);
+  new MakeLocation("Seattle Center", 11, 38, 3.7);
+  new MakeLocation("Capitol Hill", 20, 38, 2.3);
+  new MakeLocation("Alki", 2, 16, 4.6);
 }
 
+makeStores();
+console.log(allLocations)
 
+var tableEl = document.getElementById("cookieStands");
 
-
-
-
-
-
-// First and Pike Location
-// var pike = {
-//   name: '1st and Pike',
-//   minCustHr: 23,
-//   maxCustHr: 65,
-//   avgSalePerCust: 6.3,
-//   randCustHr: [],
-//   cookiesHr: [],
-//   totalCookies: 0,
-//   // method for random customers by hour
-//   calcRandCustHr: function() {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.randCustHr.push(Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr);
-//     // console.log(this.randCustHr[i]);
-//     }
-//   },
-//   // method for cookies sold by hour
-//   calcCookiesHr: function() {
-//     for (var j=0; j < hours.length; j++) {
-//       this.cookiesHr.push(Math.round(this.avgSalePerCust * this.randCustHr[j]));
-//     // console.log(this.cookiesHr[j]);
-//     }
-//   },
-//   // render function into lists
-//   render: function () {
-//     var pike = document.getElementById('pike');
-//     var pikeList = document.getElementById('pikeList');
-//     this.calcRandCustHr();
-//     this.calcCookiesHr();
-//     var h3El = document.createElement('h3');
-//     h3El.textContent = this.name;
-//     pike.appendChild(h3El);
-//     for (var k = 0; k < hours.length; k++) {
-//       var liEl = document.createElement('li');
-//       liEl.textContent = hours[k] + ': ' + this.cookiesHr[k] + ' cookies';
-//     console.log(liEl);
-//     pikeList.appendChild(liEl);
-//     }
-//   }
-// };
-// pike.render();
-
-// var seatac = {
-//   name: 'SeaTac Airport',
-//   minCustHr: 3,
-//   maxCustHr: 24,
-//   avgSalePerCust: 1.2,
-//   randCustHr: [],
-//   cookiesHr: [],
-//   totalCookies: 0,
-
-//   calcRandCustHr: function() {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.randCustHr.push(Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr);
-//     // console.log(this.randCustHr[i]);
-//     }
-//   },
-  
-//   calcCookiesHr: function() {
-//     for (var j=0; j < hours.length; j++) {
-//       this.cookiesHr.push(Math.round(this.avgSalePerCust * this.randCustHr[j]));
-//     // console.log(this.cookiesHr[j]);
-//     }
-//   },
-  
-//   render: function () {
-//     var seatac = document.getElementById('seatac');
-//     var seatacList = document.getElementById('seatacList');
-//     this.calcRandCustHr();
-//     this.calcCookiesHr();
-//     var h3El = document.createElement('h3');
-//     h3El.textContent = this.name;
-//     seatac.appendChild(h3El);
-//     for (var k = 0; k < hours.length; k++) {
-//       var liEl = document.createElement('li');
-//       liEl.textContent = hours[k] + ': ' + this.cookiesHr[k] + ' cookies';
-//     console.log(liEl);
-//     seatacList.appendChild(liEl);
-//     }
-//   }
-// };
-// seatac.render();
-
-// var seaCenter = {
-//   name: 'Seattle Center',
-//   minCustHr: 11,
-//   maxCustHr: 38,
-//   avgSalePerCust: 3.7,
-//   randCustHr: [],
-//   cookiesHr: [],
-//   totalCookies: 0,
-
-//   calcRandCustHr: function() {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.randCustHr.push(Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr);
-//     // console.log(this.randCustHr[i]);
-//     }
-//   },
-  
-//   calcCookiesHr: function() {
-//     for (var j=0; j < hours.length; j++) {
-//       this.cookiesHr.push(Math.round(this.avgSalePerCust * this.randCustHr[j]));
-//     // console.log(this.cookiesHr[j]);
-//     }
-//   },
-  
-//   render: function () {
-//     var seaCenter = document.getElementById('seaCenter');
-//     var seaCenterList = document.getElementById('seaCenterList');
-//     this.calcRandCustHr();
-//     this.calcCookiesHr();
-//     var h3El = document.createElement('h3');
-//     h3El.textContent = this.name;
-//     seaCenter.appendChild(h3El);
-//     for (var k = 0; k < hours.length; k++) {
-//       var liEl = document.createElement('li');
-//       liEl.textContent = hours[k] + ': ' + this.cookiesHr[k] + ' cookies';
-//     console.log(liEl);
-//     seaCenterList.appendChild(liEl);
-//     }
-//   }
-// };
-// seaCenter.render();
-
-// var capHill = {
-//   name: 'Capitol Hill',
-//   minCustHr: 20,
-//   maxCustHr: 38,
-//   avgSalePerCust: 2.3,
-//   randCustHr: [],
-//   cookiesHr: [],
-//   totalCookies: 0,
-
-//   calcRandCustHr: function() {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.randCustHr.push(Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr);
-//     // console.log(this.randCustHr[i]);
-//     }
-//   },
-  
-//   calcCookiesHr: function() {
-//     for (var j=0; j < hours.length; j++) {
-//       this.cookiesHr.push(Math.round(this.avgSalePerCust * this.randCustHr[j]));
-//     // console.log(this.cookiesHr[j]);
-//     }
-//   },
-  
-//   render: function () {
-//     var capHill = document.getElementById('capHill');
-//     var capHillList = document.getElementById('capHillList');
-//     this.calcRandCustHr();
-//     this.calcCookiesHr();
-//     var h3El = document.createElement('h3');
-//     h3El.textContent = this.name;
-//     capHill.appendChild(h3El);
-//     for (var k = 0; k < hours.length; k++) {
-//       var liEl = document.createElement('li');
-//       liEl.textContent = hours[k] + ': ' + this.cookiesHr[k] + ' cookies';
-//     console.log(liEl);
-//     capHillList.appendChild(liEl);
-//     }
-//   }
-// };
-// capHill.render();
-
-// var alki = {
-//   name: 'Alki',
-//   minCustHr: 2,
-//   maxCustHr: 16,
-//   avgSalePerCust: 4.6,
-//   randCustHr: [],
-//   cookiesHr: [],
-//   totalCookies: 0,
-
-//   calcRandCustHr: function() {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.randCustHr.push(Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr);
-//     // console.log(this.randCustHr[i]);
-//     }
-//   },
-  
-//   calcCookiesHr: function() {
-//     for (var j=0; j < hours.length; j++) {
-//       this.cookiesHr.push(Math.round(this.avgSalePerCust * this.randCustHr[j]));
-//     // console.log(this.cookiesHr[j]);
-//     }
-//   },
-  
-//   render: function () {
-//     var alki = document.getElementById('alki');
-//     var alkiList = document.getElementById('alkiList');
-//     this.calcRandCustHr();
-//     this.calcCookiesHr();
-//     var h3El = document.createElement('h3');
-//     h3El.textContent = this.name;
-//     alki.appendChild(h3El);
-//     for (var k = 0; k < hours.length; k++) {
-//       var liEl = document.createElement('li');
-//       liEl.textContent = hours[k] + ': ' + this.cookiesHr[k] + ' cookies';
-//     // console.log(liEl);
-//     alkiList.appendChild(liEl);
-//     }
-//   }
-// };
-// alki.render();
+function makeHeaderRow() {
+  var trEl = document.createElement("tr");
+  for (var i = 0; i < hours.length; i++) {
+    var thEl = document.createElement("th");
+    thEl.textContent = hours[i];
+    trEl.appendChild(thEl);
+  }
+  tableEl.appendChild(trEl);
+};
+makeHeaderRow(hours);
